47608c3dc2b1021c1e9b53f0ace923dab45495c1
Pachy2007 <139348227+Pachy2007@users.noreply.github.com>
2024-07-22 16:24:41 +0300

    cosmin

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Modules/Drive/MecanumDriveTrain.java
@@ -0,0 +1,2 @@
+package org.firstinspires.ftc.teamcode.Modules.Drive;public class MecanumDriveTrain {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/BlueRight.java
@@ -0,0 +1,93 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Modules.Drive.MecanumDriveTrain;
+import org.firstinspires.ftc.teamcode.Modules.Intake.Extendo;
+import org.firstinspires.ftc.teamcode.Modules.Others.BottomGripper;
+import org.firstinspires.ftc.teamcode.Modules.Others.TopGripper;
+import org.firstinspires.ftc.teamcode.Modules.Outtake.Extension;
+import org.firstinspires.ftc.teamcode.Modules.Outtake.Lift;
+import org.firstinspires.ftc.teamcode.Modules.Outtake.Outtake;
+import org.firstinspires.ftc.teamcode.Robot.Hardware;
+
+
+
+public class TestTrajectory{
+
+
+    public static void run(HardwareMap hardwareMap){
+
+
+        Hardware.init(hardwareMap);
+        Hardware.imu.resetYaw();
+         MecanumDriveTrain drive=new MecanumDriveTrain(MecanumDriveTrain.State.PID);
+        Outtake outtake=new Outtake(Outtake.State.DOWN);
+        Lift.outPosition=75;
+        ElapsedTime timer=new ElapsedTime();
+        timer.startTime();
+        TopGripper topGripper=new TopGripper(TopGripper.State.OPEN);
+        BottomGripper bottomGripper=new BottomGripper(BottomGripper.State.CLOSED);
+        Extendo extendo=new Extendo(Extendo.State.GOING_IN);
+        boolean ok1=false;
+        boolean ok=false;
+        boolean ok2=false;
+        boolean ok3=false;
+        boolean ok4=false;
+
+
+        while(true)
+        {
+            drive.update();
+
+            if(ok==false)
+            {
+                outtake.goPURPLE();
+            drive.setTargetPosition(23 , -2 , -0.155);
+            ok=true;
+            }
+
+            if(drive.inPosition() && topGripper.isClosed() && ok1==false && ok==true)
+            {
+                ok1=true;
+                outtake.goDOWN();
+                drive.setTargetPosition(50 , -17 , -Math.PI/2);
+                ok1=true;
+            }            else if(drive.inPosition())topGripper.setClosed();
+
+            if(ok2==false && drive.inPosition() && ok1==true)
+            {
+                drive.setTargetPosition(50 , 60 , -Math.PI/2);
+                ok2=true;
+                topGripper.setOpen();
+                bottomGripper.setOpen();
+            }
+            if(ok2==true && drive.inPosition() && ok3==false)
+            {
+                Lift.outPosition=270;
+                drive.setTargetPosition(29 , 87.63 , -Math.PI/2);
+                outtake.goUP();
+                ok3=true;
+            }
+            if(Math.abs(outtake.lift.getPosition()-Lift.outPosition)<20 && ok3==true && drive.inPosition() && ok4==false)
+            {
+                topGripper.setClosed();
+                bottomGripper.setClosed();
+                ok4=true;
+            }
+            if(ok4==true && topGripper.isClosed() && bottomGripper.isClosed())outtake.goDOWN();
+
+
+
+            timer.reset();
+            topGripper.update();
+            bottomGripper.update();
+            outtake.update();
+            extendo.update();
+
+        }
+    }
+}
